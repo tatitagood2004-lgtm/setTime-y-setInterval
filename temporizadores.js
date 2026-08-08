7fedd65e404131c10a716ce463c7447d380741b6
Ramon Alejandro Aguirre <tatitagood2004@gmail.com>
2026-08-08 13:49:32 -0300

    nuevo arreglo

--- temporizadores.js
+++ temporizadores.js
@@ -1,18 +1,30 @@
 
 
-setTimeout(() => {
+let timeoutID = setTimeout(() => {
 console.log("¡Hola despues de 3 segundos");
 },3000);
 
+console.log("Temporizando el programa");
+
+setTimeout(() => {
+    clearTimeout(timeoutID);
+    console.log("Cancele el setTimeout");
+},1000);
+
 const interval = setInterval(() =>{
    const ahora = new Date();
-   console.log(ahora.getHours());
-   console.log(ahora.getMinutes());
-   console.log(ahora.getSeconds());
+   console.log(ahora.getHours(), ahora.getMinutes(), ahora.getSeconds());
 
 },2000);
 
 setTimeout(() =>{
 clearInterval(interval);
 },10000);
+
+
+
+
+
+
+
 
