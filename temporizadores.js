b057f63bbbe1c3e7a97caeeec2e14c501d014b50
Ramon Alejandro Aguirre <tatitagood2004@gmail.com>
2026-08-08 14:00:12 -0300

    modificacion nuevo ejercicio

--- temporizadores.js
+++ temporizadores.js
@@ -1,5 +1,28 @@
 
 
+// let timeoutID = setTimeout(() => {
+// console.log("¡Hola despues de 3 segundos");
+// },3000);
+
+// console.log("Temporizando el programa");
+
+// setTimeout(() => {
+//     clearTimeout(timeoutID);
+//     console.log("Cancele el setTimeout");
+// },1000);
+
+// const interval = setInterval(() =>{
+//    const ahora = new Date();
+//    console.log(ahora.getHours(), ahora.getMinutes(), ahora.getSeconds());
+
+// },2000);
+
+// setTimeout(() =>{
+// clearInterval(interval);
+// },10000);
+
+
+
 let timeoutID = setTimeout(() => {
 console.log("¡Hola despues de 3 segundos");
 },3000);
@@ -8,23 +31,15 @@ console.log("Temporizando el programa");
 
 setTimeout(() => {
     clearTimeout(timeoutID);
-    console.log("Cancele el setTimeout");
+    console.log("mensaje cada 1 segundo");
 },1000);
 
 const interval = setInterval(() =>{
    const ahora = new Date();
-   console.log(ahora.getHours(), ahora.getMinutes(), ahora.getSeconds());
+   console.log(ahora.getSeconds());
 
-},2000);
+},1000);
 
 setTimeout(() =>{
 clearInterval(interval);
-},10000);
-
-
-
-
-
-
-
-
+},5000);
