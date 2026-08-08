45fb5cd637a42c27b3931aa9e9b9f3cef73a2c0a
Ramon Alejandro Aguirre <tatitagood2004@gmail.com>
2026-08-08 14:18:25 -0300

    agregue texto de explicacion

--- temporizadores.js
+++ temporizadores.js
@@ -23,22 +23,39 @@
 
 
 
+// ¿Cuándo es necesario cancelarlo? Si queremos evitar que la acción ocurra
+// en el futuro (por ejemplo, porque ya no aplica o se tomó otra decisión).
+
 let timeoutID = setTimeout(() => {
 console.log("¡Hola despues de 3 segundos");
 },3000);
 
 console.log("Temporizando el programa");
 
+ //Cancelamos el setTimeout ANTES de que se cumplan los 3 segundos.
+// ¿Por qué? Para que el mensaje NO se ejecute si ya no corresponde.
+
 setTimeout(() => {
     clearTimeout(timeoutID);
     console.log("time cancelado");
 },1000);
+
+console.log("SetInterval creado (empezara ejecutandose cada 1s")
+
+// Creamos un intervalo para imprimir cada segundo.
+// ¿Cuándo conviene cancelarlo? Cuando ya cumplió su propósito (ej: después de 5s),
+// o para evitar que quede corriendo indefinidamente y siga gastando recursos.
 
 const interval = setInterval(() =>{
    const ahora = new Date();
    console.log(ahora.getSeconds());
 
 },1000);
+
+console.log("setInterval creado: empezará a ejecutar cada 1s");
+
+// Cancelamos el intervalo después de 5 segundos.
+// ¿Por qué? Porque ya no queremos seguir imprimiendo cada segundo.
 
 setTimeout(() =>{
 clearInterval(interval);
