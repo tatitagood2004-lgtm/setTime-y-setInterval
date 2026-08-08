4ee3421c6b70daa02a77e2fbdf72bb18133020a1
Ramon Alejandro Aguirre <tatitagood2004@gmail.com>
2026-08-08 14:21:41 -0300

    limpie codigo

--- temporizadores.js
+++ temporizadores.js
@@ -1,27 +1,5 @@
 
 
-// let timeoutID = setTimeout(() => {
-// console.log("¡Hola despues de 3 segundos");
-// },3000);
-
-// console.log("Temporizando el programa");
-
-// setTimeout(() => {
-//     clearTimeout(timeoutID);
-//     console.log("Cancele el setTimeout");
-// },1000);
-
-// const interval = setInterval(() =>{
-//    const ahora = new Date();
-//    console.log(ahora.getHours(), ahora.getMinutes(), ahora.getSeconds());
-
-// },2000);
-
-// setTimeout(() =>{
-// clearInterval(interval);
-// },10000);
-
-
 
 // ¿Cuándo es necesario cancelarlo? Si queremos evitar que la acción ocurra
 // en el futuro (por ejemplo, porque ya no aplica o se tomó otra decisión).
